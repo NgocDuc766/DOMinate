43c0108dc95e15d475e6be1cba7600aec509f97f
ducbnhe173558 <ducbnhe173558@fpt.edu.vn>
2025-06-02 17:49:20 +0700

    initial class and install Selenium + ChromeDriver Package

---
+++ Core/Workflow/BasicActions/BaseAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Workflow.BasicActions
+{
+    public abstract class BaseAction
+    {
+        public Guid Id { get; set; }
+        public string ActionName { get; set; }
+        public string ActionType { get; set; }
+
+    }
+}
